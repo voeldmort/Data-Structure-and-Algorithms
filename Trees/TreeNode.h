f2fcd488b3c2777280ca677466e42b53def4387f
Ranavijay Singh Suryavanshi <57328714+voeldmort@users.noreply.github.com>
2021-07-28 01:10:50 +0530

    Add files via upload

---
+++ Trees/TreeNode.h
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+public:
+    int data;
+    vector<TreeNode *> children;
+    TreeNode(int data){
+        this->data=data;
+    }
+    
+};
